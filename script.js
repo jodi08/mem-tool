70034c5b3793361883eb3f83189600eca196cef0
Jo Anna Mollman <mommollman@gmail.com>
2021-02-24 11:06:13 -0800

    start new project

---
+++ script.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('.input');
+let btn = document.querySelector('.btn');
+
+//click handler for button
+//input changed to first letters on button click
+//return first letters to input box.
+//print feature
+
+//style page to include article about memorization method.
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(input)
+})
